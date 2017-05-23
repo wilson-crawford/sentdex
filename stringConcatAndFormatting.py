e3cd3c1d02be68e4db2651ff4ab495147058463c
Wilson Crawford <wcrawford2@yahoo.com>
2017-05-22 20:20:07 -0500

    committing somemore examples

---
+++ stringConcatAndFormatting.py
@@ -0,0 +1,10 @@
+names = ['Jeff', 'Gary', 'Jill', 'Samantha']
+
+##for name in names:
+##    print ('Hello there, ' + name)
+##    print(' '.join(['Hello there,', name]))
+
+#print(', '.join(names))
+
+import os
+
